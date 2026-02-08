7b24b3aff593acd9b1911315d5fe491c10c350be
importmateoperez <mateoperezbusiness@gmail.com>
2026-02-08 18:10:36 -0500

    created Deploy_Terraform.sh

---
+++ Azure/Infra-Automation/Deploy_Terraform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Initialize Terraform
+terraform init
+
+# Format and Validate
+terraform fmt      
+terraform validate
+
+# Create a Plan
+terraform plan -out=main.tfplan
+
+# Apply the file
+terraform apply main.tfplan
