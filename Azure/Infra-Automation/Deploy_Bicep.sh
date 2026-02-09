b42485ef43d435bbc87725abb892a1008229c1ef
importmateoperez <mateoperezbusiness@gmail.com>
2026-02-09 11:02:47 -0500

    modified Deploy_Bicep.sh

--- Azure/Infra-Automation/Deploy_Bicep.sh
+++ Azure/Infra-Automation/Deploy_Bicep.sh
@@ -3,5 +3,9 @@
 # Create the Resource Group first
 az group create --name rg_prod_vm_lab --location eastus
 
+
+# Test Deployment
+az deployment group what-if --resource-group rg_prod_vm_lab --template-file VMs.bicep
+
 # Deploy your Bicep file into that group
-az deployment group create --resource-group rg_prod_vm_lab --template-file VMs.bicep
+# az deployment group create --resource-group rg_prod_vm_lab --template-file VMs.bicep
