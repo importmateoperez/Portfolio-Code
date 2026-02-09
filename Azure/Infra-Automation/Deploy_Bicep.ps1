ab8874da62db6584db1615030a40b6f0119df25e
importmateoperez <mateoperezbusiness@gmail.com>
2026-02-09 13:25:58 -0500

    modified file locations

--- Azure/Infra-Automation/Deploy_Bicep.ps1
+++ Azure/Infra-Automation/Deploy_Bicep.ps1
@@ -2,6 +2,6 @@
 $RG = "rg_prod_vm_lab"
 
 # Deploy your Bicep file into that group
-New-AzResourceGroup -Name $RG -Location westus3
+New-AzResourceGroup -Name $RG -Location northcentralus
 Get-AzResourceGroupDeploymentWhatIfResult -ResourceGroupName $RG -TemplateFile ./VMs.bicep
 # New-AzResourceGroupDeployment -ResourceGroupName exampleRG -TemplateFile ./VMs.bicep
