b79c41609801d385353e09c29ad384cee1d002b0
importmateoperez <mateoperezbusiness@gmail.com>
2026-07-13 14:16:45 -0400

    Cleaned projects

--- Azure/Infra-Automation/Deploy_Bicep.ps1
+++
@@ -1,7 +0,0 @@
-# Create the Resource Group first
-$RG = "rg_prod_vm_lab"
-
-# Deploy your Bicep file into that group
-New-AzResourceGroup -Name $RG -Location northcentralus
-Get-AzResourceGroupDeploymentWhatIfResult -ResourceGroupName $RG -TemplateFile ./VMs.bicep
-# New-AzResourceGroupDeployment -ResourceGroupName exampleRG -TemplateFile ./VMs.bicep
