a961f07b8c50598bc2fa91fe5da74dc005a81d29
importmateoperez <mateoperezbusiness@gmail.com>
2026-07-15 12:39:50 -0400

    Created deployment script for vnet

--- Azure/Bicep/Deploy Scripts/deployNetwork.sh
+++ Azure/Bicep/Deploy Scripts/deployNetwork.sh
@@ -1,10 +1,22 @@
-#Login only if needed
+# Login only if needed
 az login
 
-#Select subscription
+# Select subscription
 az account set --subscription "e9567da1-99bc-41c0-8e86-f168bd848ac8"
 
-#Deploy
+# Validation
+az deployment group validate \
+    --resource-group "New_Test_Group" \
+    --template-file "../main.bicep" \
+    --parameters "../environments/dev/dev.bicepparam"
+
+# Preview
+az deployment group what-if \
+    --resource-group "New_Test_Group" \
+    --template-file "../main.bicep" \
+    --parameters "../environments/dev/dev.bicepparam"
+
+# Deploy
 az deployment group create \
     --resource-group New_Test_Group \
     --template-file ../main.bicep \
