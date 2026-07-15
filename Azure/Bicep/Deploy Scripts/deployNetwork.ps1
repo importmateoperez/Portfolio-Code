eab16d87e66ce6d1515fe76de1fb6f96a9bca06b
importmateoperez <mateoperezbusiness@gmail.com>
2026-07-15 13:21:15 -0400

    Changed to powershell script

---
+++ Azure/Bicep/Deploy Scripts/deployNetwork.ps1
@@ -0,0 +1,35 @@
+# Variables
+$subscriptionId = "e9567da1-99bc-41c0-8e86-f168bd848ac8"
+$resourceGroup = "New_Test_Group"
+$templateFile = "../main.bicep"
+$parameterFile = "../environments/dev/dev.bicepparam"
+
+# Login
+az login
+
+# Select subscription
+az account set --subscription $subscriptionId
+
+# Validate
+Write-Host "Validating deployment..."
+
+az deployment group validate `
+    --resource-group $resourceGroup `
+    --template-file $templateFile `
+    --parameters $parameterFile
+
+# What-if
+Write-Host "Checking changes..."
+
+az deployment group what-if `
+    --resource-group $resourceGroup `
+    --template-file $templateFile `
+    --parameters $parameterFile
+
+# Deploy
+Write-Host "Deploying..."
+
+az deployment group create `
+    --resource-group $resourceGroup `
+    --template-file $templateFile `
+    --parameters $parameterFile
