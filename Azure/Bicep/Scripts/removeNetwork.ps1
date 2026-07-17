29536860758a1f4f5ab3d81e6d4ff7f00cc69482
importmateoperez <mateoperezbusiness@gmail.com>
2026-07-17 11:06:25 -0400

    Added delete script

--- Azure/Bicep/Scripts/removeNetwork.ps1
+++ Azure/Bicep/Scripts/removeNetwork.ps1
@@ -5,10 +5,23 @@ $subscriptionId = "e9567da1-99bc-41c0-8e86-f168bd848ac8"
 #Login to Azure
 az account set --subscription $subscriptionId
 
-$rgParams = @{
-    Name = $resourceGroupName
-    Force = $true
-}
+# Confirm resource group exists
+az group show --name $resourceGroupName
 
-# Delete Entire Resource Group
-Remove-AzResourceGroup @rgParams
+# Confirm deletion
+$confirmation = Read-Host "Type DELETE to remove $resourceGroupName"
+
+if ($confirmation -eq "DELETE") {
+
+    Write-Host "Deleting resource group $resourceGroupName..."
+
+    az group delete `
+        --name $resourceGroupName `
+        --yes
+
+    Write-Host "Deletion started."
+
+}
+else {
+    Write-Host "Deletion cancelled."
+}
