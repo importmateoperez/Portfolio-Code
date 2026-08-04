5eda8c990d28f7e8b2ace6e3a8a6573b0a903522
importmateoperez <mateoperezbusiness@gmail.com>
2026-08-04 15:30:35 -0400

    Added check for resource group

--- Azure/Bicep/Scripts/deployNetwork.ps1
+++ Azure/Bicep/Scripts/deployNetwork.ps1
@@ -1,14 +1,26 @@
 # Variables
 $subscriptionId = "e9567da1-99bc-41c0-8e86-f168bd848ac8"
 $resourceGroup = "New_Test_Group"
-$templateFile = "../main.bicep"
-$parameterFile = "../Environments/Dev/dev.bicepparam"
+$location = "westus2" 
+$templateFile = "/main.bicep"
+$parameterFile = "/Environments/Dev/dev.bicepparam"
 
 # Login
 az login
 
 # Select subscription
 az account set --subscription $subscriptionId
+
+# Check if Resource Group exists
+Write-Host "Checking if resource group '$resourceGroup' exists..."
+$rgExists = az group exists --name $resourceGroup
+
+if ($rgExists -eq 'false') {
+    Write-Host "Resource group '$resourceGroup' not found. Creating it in '$location'..."
+    az group create --name $resourceGroup --location $location
+} else {
+    Write-Host "Resource group '$resourceGroup' already exists."
+}
 
 # Validate
 Write-Host "Validating deployment..."
